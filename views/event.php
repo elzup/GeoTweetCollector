2d7f972282b08b8d4e8fd91910c308f0a4667d0f
elzzup <guild0105@gmail.com>
2015-05-14 09:11:46 +0900

    view markers

--- views/event.php
+++ views/event.php
@@ -56,8 +56,8 @@ var center = {lat: 35.6521438, lng: 139.7021483};
         })
 //        bounds.extend(pos);
 
-        var col = id_to_color(data[i].id);
-        set_marker(col, data[i].lat, data[i].lng, map, infowindow, data[i].timestamp, data[i].text + "[" + data[i].timestamp + "]");
+        var col = id_to_color(data[i].id, data[i].length);
+        set_marker(col, data[i].pos[0], data[i].pos[1], map, infowindow, data[i].timestamp, data[i].text + "[" + data[i].timestamp + "]");
  
     }
 //    map.fitBounds(bounds); //全てのデータが画面に収まる様に表示を変更
@@ -90,9 +90,64 @@ function set_marker(col, lat, lon, map, infowindow, time, text) {
     })(marker, time));
 }
 
-function id_to_color(id) {
-    return '#ffffff';
-//    return ['#ff0000', '#00ff00', '#0000ff', 'ffff00', 'ff00ff', '00ffff', 'ffffff', '888888', '000000'][id];
+function id_to_color(id, max) {
+    h = 360 * id / max;
+    rgb = HSVtoRGB(h, 50, 100);
+
+    console.log(id);
+//    return '#ffffff';
+    return ['ff0000', '00ff00', '0000ff', 'ffff00', 'ff00ff', '00ffff', 'ffffff', '888888', '000000'][id];
+}
+
+
+/**
+ * HSV配列 を RGB配列 へ変換します
+ *
+ * @param   {Number}  h         hue値        ※ 0～360の数値
+ * @param   {Number}  s         saturation値 ※ 0～255 の数値
+ * @param   {Number}  v         value値      ※ 0～255 の数値
+ * @return  {Object}  {r, g, b} ※ r/g/b は 0～255 の数値
+ */
+function HSVtoRGB (h, s, v) {
+  var r, g, b; // 0..255
+
+  while (h < 0) {
+    h += 360;
+  }
+
+  h = h % 360;
+
+  // 特別な場合 saturation = 0
+  if (s == 0) {
+    // → RGB は V に等しい
+    v = Math.round(v);
+    return {'r': v, 'g': v, 'b': v};
+  }
+
+  s = s / 255;
+
+  var i = Math.floor(h / 60) % 6,
+      f = (h / 60) - i,
+      p = v * (1 - s),
+      q = v * (1 - f * s),
+      t = v * (1 - (1 - f) * s)
+
+  switch (i) {
+    case 0 :
+      r = v;  g = t;  b = p;  break;
+    case 1 :
+      r = q;  g = v;  b = p;  break;
+    case 2 :
+      r = p;  g = v;  b = t;  break;
+    case 3 :
+      r = p;  g = q;  b = v;  break;
+    case 4 :
+      r = t;  g = p;  b = v;  break;
+    case 5 :
+      r = v;  g = p;  b = q;  break;
+  }
+
+  return {'r': Math.round(r), 'g': Math.round(g), 'b': Math.round(b)};
 }
 
 
